775776e4c21feaf8a8e248ca937c05e015791676
RahmatullahWiryaAdikusuma <awirya816@gmail.com>
2022-09-06 21:20:12 +0700

    Praktikum_web_lanjut_2

---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+//use App\Http\Controllers\ProductController;
+//use App\Http\Controllers\NewsController;
+//use App\Http\Controllers\ProgramController;
+//use App\Http\Controllers\AboutUsController;
+//use App\Http\Controllers\ContactUsController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+ Route::get('/', function () {
+    echo view('welcome');
+ });
+ Route::get('/about', function () {
+     echo 'NIM: 06.2020.1.07349 <br> Nama : Rahmatullah Wirya Adikusuma <br> Kelas : Web-B';
+ });
+ Route::get('/articles/{id}', function ($id) {
+     echo 'Halaman artikel ini dengan id ' . $id;
+ });
+
+ 
+
+ Route::get('/', [HomeController::class, 'index']);
+ Route::get('/about', [AboutController::class, 'index']);
+ Route::get('/articles/{id}', [ArticleController::class, 'show']);
+
+
+
+//Route::get('/home', function () {
+//    echo '<h1>Halaman Awal Website<h1>';
+//});
+
+//Route::prefix('category')->group(function () {
+ //   route::get('/{id}', [ProductController::class, 'product']);
+//});
+
+
+//Route::get('/news/{id?}', [NewsController::class, 'news']);
+
+//Route::prefix('program')->group(function () {
+   // route::get('/{id}', [ProgramController::class, 'program']);
+//});
+
+//Route::get('/AboutUs', [AboutUsController::class, 'about']);
+
+//Route::resource('/contactUs', ContactUsController::class);
